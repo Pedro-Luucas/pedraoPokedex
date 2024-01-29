0105f8b2cb2ac7b526d1446c34d94a9052f66232
Pedro Lucas <pedrolucas1101@proton.me>
2024-01-29 19:21:33 -0300

    muito erro, converter dependencia, muita coisa que eu nao faco ideia como resolver, mais tarde resolvo.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pedraoPokedex
-pokedex seguindo o tutorial do readme
+pedraPokedex usando a pokeapi,firebase e typescript
+
+
+
+seguindo esse tutorial:
+
+https://www.youtube.com/watch?v=fJnBwmuKJhw&list=PLubMkQeK3jbktqHAx2pqxM-HVA_0tt662&index=11
---
+++ pedraoPokedex/src/pages/welcome/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as S from './styles'
+
+export function Welcome() {
+    
+
+
+    return(
+        <S.Container>
+            <S.Content>
+
+            </S.Content>
+
+            <S.Footer>
+
+            </S.Footer>
+        </S.Container>
+    );
+}
